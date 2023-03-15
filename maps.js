52a2daadac990acedbb0eaa1cce79c1d0df47d0a
jiffylube <lu.jeffrey91@gmail.com>
2023-03-15 09:25:48 -0400

    maps

--- maps.js
+++ maps.js
@@ -1,11 +1,29 @@
 // javascript objects only support one key object pair
 // maps solve this issue, can have multiple values per key
+// both key values below contain both a, and b as seperate objects
 
-const myMap = new Map(
-  [
-    ["john", "doe"],
-    ["jeff", "chen"]
-  ]
-)
+const a = {}
+const b = {}
 
+const myMap = new Map([[a, 'a'], [b, 'b']])
 console.log(myMap)
+
+// set method -> adds a new key value pair
+myMap.set('ron', 12)
+myMap.set('jake', 25)
+console.log(myMap)
+
+// delete method -> deletes the key value pair
+myMap.delete('ron')
+console.log(myMap)
+
+// clear method -> clears whole map
+// myMap.clear()
+
+// has method -> output true/false by searching map through key values
+console.log(myMap.has('ron'))
+console.log(myMap.has('jake'))
+
+// size method -> outputs how many elements the map has
+
+console.log(myMap.size)
