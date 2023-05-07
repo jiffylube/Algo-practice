b788bb10501bc25efbf600a37092346f65d0e839
jiffylube <lu.jeffrey91@gmail.com>
2023-05-07 19:18:37 -0400

    hard!

---
+++ problems/trappingWater.js
@@ -0,0 +1,27 @@
+// 42 leet code trapping water
+// Given n non-negative integers representing an elevation map where the width of each bar is 1
+// compute how much water it can trap after raining.
+
+// Constraints:
+
+// n == height.length
+// 1 <= n <= 2 * 104
+// 0 <= height[i] <= 105
+
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// output is area of water allowed
+
+let trap = function (height) {
+
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+
+  while (left <= right) {
+  }
+
+};
+
+trap([5,3,0,2,4])
